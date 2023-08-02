899e6124fd9e22d2f374de86534a926a55191b84
akos borbely <macos.borbely@gmail.com>
2023-08-02 10:46:32 +0200

    init paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,36 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout';
+import LandingPage from './pages/LandingPage';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'skills',
+        element: <Skills />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
