4fdaaeafafb2e7fd1ad1861cb86ea3e581c7db75
akos borbely <macos.borbely@gmail.com>
2023-08-03 13:53:27 +0200

    landing page css init and add

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,32 @@
+import { Link } from 'react-router-dom';
+import './LandingPage.css';
+
 function LandingPage() {
-  return <h1>This is landing page!</h1>;
+  return (
+    <section className="landing-page-container">
+      <div className="introduction-container">
+        <h1>
+          Hi, I'm <span>Ákos Borbély</span>
+        </h1>
+        <div className="animated-text">
+          <h3>Fullstack Developer</h3>
+        </div>
+        <p>
+          Welcome to my portfolio website! Im a passionate Fullstack Developer
+          showcasing my creative work, projects, and accomplishments. Explore my
+          diverse portfolio and if you feel like it, feel free to contact me.
+        </p>
+        <div className="btn-container">
+          <Link to="#" className="main-btn">
+            Hire me
+          </Link>
+          <Link to="#" className="main-btn">
+            Contact
+          </Link>
+        </div>
+        <div className="home-img-hover" />
+      </div>
+    </section>
+  );
 }
 export default LandingPage;
