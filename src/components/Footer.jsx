14a3b7f23823b70bea1a46c51ea27ee9adaf6455
akos borbely <macos.borbely@gmail.com>
2023-08-03 13:53:04 +0200

    footer css init and add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,23 @@
+import { CiFacebook, CiLinkedin } from 'react-icons/ci';
+import { BsGithub } from 'react-icons/bs';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
 function Footer() {
-  return <h1>This is footer component!</h1>;
+  return (
+    <footer>
+      <div className="social-links-container">
+        <Link to="https://www.facebook.com/akos.borbely.71/" target="_blank">
+          <CiFacebook size={50} color="#00abf0" />
+        </Link>
+        <Link to="https://github.com/AkosBrb" target="_blank">
+          <BsGithub size={42} color="#00abf0" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/akos-borbely/" target="_blank">
+          <CiLinkedin size={50} color="#00abf0" />
+        </Link>
+      </div>
+    </footer>
+  );
 }
 export default Footer;
