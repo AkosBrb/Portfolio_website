3282807fc975f056195bd22e8154a2f3eb743cbd
akos borbely <macos.borbely@gmail.com>
2023-08-03 13:53:14 +0200

    header css init and add

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
 function Header() {
-  return <h1>This is header component!</h1>;
+  return (
+    <header className="header-container">
+      <p className="logo-name">Ákos.</p>
+      <nav>
+        <ul className="nav-links-container">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="skills">Skills</NavLink>
+          </li>
+          <li>
+            <NavLink to="contact">Contact</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 export default Header;
