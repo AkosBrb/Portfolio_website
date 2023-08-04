eec80c3622a8b10a8b96d7bf4db9722ca6d91c87
akos borbely <macos.borbely@gmail.com>
2023-08-04 11:29:10 +0200

    refactoring layout

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,18 @@
-import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import LandingPage from './pages/LandingPage';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Contact from './pages/Contact';
 
 function Layout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <LandingPage />
+      <About />
+      <Skills />
+      <Contact />
       <Footer />
     </>
   );
